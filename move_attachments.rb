1c0d505e5851f4edc2c30609cbaf172439b593ef
Bunyamin <inanbunyamin90@gmail.com>
2016-10-08 22:43:15 +0300

    Move attachments task is added.

---
+++ move_attachments.rb
@@ -0,0 +1,27 @@
+# Change paperclip attachment :path
+# 
+# This task moves your attachements from old :path to new :path.
+# I used lots of interpolations in old :path, so in order to
+# move attachments correctly, I had to used interpolations explicitly.
+# 
+# Apparently, Paperclip::Attachement.interpolate method is private. So
+# I copied its body, which is interpolator.interpolate(pattern, self, style_name)
+namespace :paperclip do
+  task :move_attachments => :environment do
+    News.find_each do |item|
+        old_pattern = "#{Rails.root}/public/system/:class/:id_partition/:style/:id.:extension"
+        
+        filename = Paperclip::Interpolations.interpolate(old_pattern, item.image, :original)
+        if File.exists? filename
+          puts "Re-saving image attachment #{item.id} - #{filename}"
+          image = File.new filename
+          item.image = image
+          item.save
+          
+          # if there are multiple styles, you want to recreate them :
+          post.image.reprocess! 
+          image.close
+        end
+    end
+  end
+end
